50bca0980fc14d66f74d6a2a6691af70892a33bd
Joe Tomlin <joetomlin8@gmail.com>
2024-02-18 23:15:36 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Left from './components/LeftScreen'
+
+
+function App() {
+
+  return (
+    <>
+      <Left/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LeftScreen.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./LeftScreen.css";
+
+const Left = () => {
+
+    return (
+        <div>
+            <h1 className='h1'>JOE TOMLIN</h1>
+            <h3 className='h3'>Full Stack Software Engineer</h3>
+            <div>
+                <link rel="shortcut icon" href="https://www.linkedin.com/in/josephtomlin/" type="image/x-icon" />
+                <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+                <a href="https://github.com/JoeTomlin" className='icon'><img src="src/images/github_logo.png" alt="Joe's GitHub" className='icon' /></a>
+                <a href="https://www.linkedin.com/in/josephtomlin/" className='icon'><img src="src/images/linkedin_logo.png" alt="Joe's LinkedIn" className='icon' /></a>
+                <a href="https://www.instagram.com/jouktomlin/" className='icon'><img src="src/images/instagram_logo.png" alt="Joe's Insta" className='icon'/></a>
+            </div>
+            <img className='profile' src="src/images/joe_profile_pic.jpeg" alt="Pic of Joe" />
+        </div>
+    )
+}
+
+export default Left
